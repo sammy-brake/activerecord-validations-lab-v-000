a6aac88ddfe4c99ece105255ea028f4d8d8a6598
Samantha brake <sammy.brake@gmail.com>
2019-04-06 02:23:23 +0000

    Automatically backed up by Learn

--- app/models/post.rb
+++ app/models/post.rb
@@ -4,6 +4,6 @@ class Post < ActiveRecord::Base
   validates :summary, length: { maximum: 250 }
   validates :category, inclusion: { in: %w(Fiction Nonfiction) }
 
-def validate 
-end 
+def validate
+end
 end
