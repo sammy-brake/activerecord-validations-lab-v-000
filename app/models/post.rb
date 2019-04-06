bbf5817156f843350ca424e73d48ef0efbf9030d
Samantha brake <sammy.brake@gmail.com>
2019-04-06 02:42:13 +0000

    Automatically backed up by Learn

--- app/models/post.rb
+++ app/models/post.rb
@@ -7,7 +7,7 @@ class Post < ActiveRecord::Base
 
 
   def clickbait
-    binding.pry 
+    binding.pry
     if !(self.title.include?("Won't Believe") || self.title.include?( "Secret") || self.title.include?("Top [number]") || self.title.include?("Guess"))
       errors.add(:title, "not clickbate")
     end
